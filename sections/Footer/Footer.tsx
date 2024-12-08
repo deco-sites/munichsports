30edb3a2ed9948dab0a1be55e69b7e31f4af0f83
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-12-08 13:34:10 -0400

    almost there with mobile footer uhul

--- sections/Footer/Footer.tsx
+++ sections/Footer/Footer.tsx
@@ -1,55 +1,46 @@
-import { type ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
-import PoweredByDeco from "apps/website/components/PoweredByDeco.tsx";
 import Icon from "../../components/ui/Icon.tsx";
 import { extractLanguagesProps } from "../../sdk/i18n.ts";
+import type { ImageProps } from "../../sdk/widgets.ts";
 
-/** @titleBy title */
+/** @titleBy label */
 interface Item {
-  title: string;
+  label: string;
   href: string;
-}
-
-/** @titleBy title */
-interface Link extends Item {
-  children: Item[];
 }
 
 /** @titleBy alt */
 interface Social {
   alt?: string;
   href?: string;
-  image: ImageWidget;
+  image: Omit<ImageProps, "alt">;
+}
+
+/** @titleBy label */
+interface App extends Item {
+  image: ImageProps;
 }
 
 interface Props {
-  links?: Link[];
   social?: Social[];
-  paymentMethods?: Social[];
+  links?: Item[];
+  apps?: App[];
+  copyright?: string;
   policies?: Item[];
-  logo?: ImageWidget;
-  trademark?: string;
 }
 
 export default function Footer(props: Props) {
   const {
     links = [],
     social = [],
+    apps = [],
     policies = [],
-    paymentMethods = [],
-    logo,
-    trademark,
+    copyright,
   } = props;
 
-  const { translations, language, supportedLanguages } = extractLanguagesProps(
+  const { translations } = extractLanguagesProps(
     props,
   );
-  console.log({
-    translations,
-    language,
-    supportedLanguages,
-  });
-
   return (
     <>
       <button
@@ -62,79 +53,59 @@ export default function Footer(props: Props) {
         <Icon id="to-top-arrow" size={20} />
       </button>
       <footer
-        class="px-5"
-        style={{ backgroundColor: "#EFF0F0" }}
+        class="px-5 flex flex-col py-6"
+        style={{ backgroundColor: "#ffffff" }}
       >
-        <div class="container flex flex-col gap-5 sm:gap-10 py-10">
-          <ul class="grid grid-flow-row sm:grid-flow-col gap-6 ">
-            {links.map(({ title, href, children }) => (
-              <li class="flex flex-col gap-4">
-                <a class="text-base font-semibold" href={href}>{title}</a>
-                <ul class="flex flex-col gap-2">
-                  {children.map(({ title, href }) => (
-                    <li>
-                      <a class="text-sm font-medium text-base-400" href={href}>
-                        {title}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            ))}
-          </ul>
-          <div class="flex flex-col sm:flex-row gap-12 justify-between items-start sm:items-center">
-            <ul class="flex gap-4">
-              {social.map(({ image, href, alt }) => (
-                <li>
-                  <a href={href}>
-                    <Image
-                      src={image}
-                      alt={alt}
-                      loading="lazy"
-                      width={24}
-                      height={24}
-                    />
-                  </a>
-                </li>
-              ))}
-            </ul>
-            <ul class="flex flex-wrap gap-2">
-              {paymentMethods.map(({ image, alt }) => (
-                <li class="h-8 w-10 border border-base-100 rounded flex justify-center items-center">
-                  <Image
-                    src={image}
-                    alt={alt}
-                    width={20}
-                    height={20}
-                    loading="lazy"
-                  />
-                </li>
-              ))}
-            </ul>
-          </div>
-          <hr class="w-full text-base-400" />
-          <div class="grid grid-flow-row sm:grid-flow-col gap-8">
-            <ul class="flex flex-col sm:flex-row gap-2 sm:gap-4 sm:items-center">
-              {policies.map(({ title, href }) => (
-                <li>
-                  <a class="text-xs font-medium" href={href}>
-                    {title}
-                  </a>
-                </li>
-              ))}
-            </ul>
-            <div class="flex flex-nowrap items-center justify-between sm:justify-center gap-4">
-              <div>
-                <img loading="lazy" src={logo} />
-              </div>
-              <span class="text-xs font-normal text-base-400">{trademark}</span>
-            </div>
-            <div class="flex flex-nowrap items-center justify-center gap-4">
-              <span class="text-sm font-normal text-base-400">Powered by</span>
-              <PoweredByDeco />
-            </div>
-          </div>
-        </div>
+        <ul class="flex justify-center w-full items-center gap-2">
+          {social.map(({ image, alt, href }) => (
+            <li>
+              <a href={href}>
+                <Image
+                  src={image.src}
+                  alt={alt || "Social icon"}
+                  width={image.width || 0}
+                  height={image.height}
+                  loading="lazy"
+                />
+              </a>
+            </li>
+          ))}
+        </ul>
+        <ul class="flex flex-col py-6 items-center justify-center text-[13px] font-bold text-[#454545] leading-[13px]">
+          {links.map(({ href, label }) => (
+            <li>
+              <a href={href} class="px-4 py-2 inline-block w-full">
+                {label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <ul class="flex flex-col justify-center items-center gap-4">
+          {apps.map(({ image, label }) => (
+            <li class="flex flex-col items-center justify-center gap-4">
+              <Image
+                src={image.src}
+                alt={image.alt || "App icon"}
+                width={image.width || 0}
+                height={image.height}
+                loading="lazy"
+              />
+              <span class="text-sm/[14px] font-bold text-[#9a9a9a]">
+                {label}
+              </span>
+            </li>
+          ))}
+        </ul>
+        <hr class="w-full text-base-400" />
+        <ul class="flex flex-col sm:flex-row gap-2 sm:gap-4 sm:items-center">
+          {policies.map(({ label, href }) => (
+            <li>
+              <a class="text-xs font-medium" href={href}>
+                {label}
+              </a>
+            </li>
+          ))}
+        </ul>
       </footer>
     </>
   );
